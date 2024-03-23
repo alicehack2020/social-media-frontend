a101d49830411fc0a9f5b426e222350923468a9b
alicehack2020 <alicehack2020@gmail.com>
2024-03-23 23:47:02 +0530

    outlet added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,57 @@
 import React from "react";
+import { Outlet, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Home } from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import NavBar from "./components/NavBar";
+import Leftbar from "./components/Leftbar";
+import SideBar from "./components/SideBar";
+import Profile from "./pages/Profile";
 
 const App = () => {
-  return <div className="text-red-900">App</div>;
+  const Layout = () => {
+    return (
+      <div>
+        <NavBar />
+        <div className="flex">
+          <Leftbar />
+          <Outlet />
+          <SideBar />
+        </div>
+      </div>
+    );
+  };
+
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      path: "/",
+      children: [
+        {
+          element: <Home />,
+          path: "/",
+        },
+        {
+          element: <Profile />,
+          path: "/profile/:id",
+        },
+      ],
+    },
+    {
+      element: <Login />,
+      path: "/login",
+    },
+    {
+      element: <Register />,
+      path: "/register",
+    },
+  ]);
+
+  return (
+    <di>
+      <RouterProvider router={router} />
+    </di>
+  );
 };
 
 export default App;
